617f4054eb8444e9b13d8500ade8601559da9570
Jan Scheres Monesma <jscheresm@gmail.com>
2025-11-25 19:56:17 +0000

    gitignore __marimo__ folder

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from compproject!")
+import marimo
+
+__generated_with = "0.18.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import time
+    return
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
